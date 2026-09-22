bff2e53d1bd679ae585f1191483cf04464b40aa3
paging <wfbq0xtw@anonaddy.com>
2026-09-22 03:11:16 -0400

    Grant archiver Kinesis and S3 access

--- infra/iam.tf
+++ infra/iam.tf
@@ -187,3 +187,42 @@ resource "aws_iam_role_policy" "incident_analyzer" {
     ]
   })
 }
+
+resource "aws_iam_role" "archiver" {
+  name = "${local.name_prefix}-archiver-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "archiver" {
+  name = "${local.name_prefix}-archiver-policy"
+  role = aws_iam_role.archiver.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["kinesis:GetRecords", "kinesis:GetShardIterator", "kinesis:DescribeStream", "kinesis:ListShards"]
+        Resource = aws_kinesis_stream.events.arn
+      },
+      {
+        Effect = "Allow"
+        Action = ["s3:PutObject"]
+        Resource = "${aws_s3_bucket.event_archive.arn}/*"
+      },
+      {
+        Effect = "Allow"
+        Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+        Resource = "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:*"
+      }
+    ]
+  })
+}
